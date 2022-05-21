1cd765b6374ba76dc74bd4c7e957b1f4407c1b28
Flavioeds82 <flavio_eduardo@id.uff.br>
2022-05-21 20:59:48 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import styles from '*.module.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className={styles.main}>
      ***teste***
     </div>
   )
